9b684b422b07c4042d63135e2f868754625925e0
Alexa S <alexasteinbrueck@gmail.com>
2020-02-13 15:06:40 +0100

    Add NumberBullet component

---
+++ src/components/NumberBullet.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dib">
+    <div
+      class="bg-berry br-100 w2 h2 flex white justify-center items-center sans-serif"
+    >
+      <span>{{ number }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import "../css/global-styles.scss";
+export default {
+  name: "NumberBullet",
+  props: { number: { type: String } }
+};
+</script>
