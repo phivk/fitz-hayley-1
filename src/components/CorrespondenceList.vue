43e164fe69619274c382a32b341c7a493f188dc0
Philo van Kemenade <philovankemenade@gmail.com>
2020-03-07 17:37:10 +0100

    implement CorrespondenceList molecule

---
+++ src/components/CorrespondenceList.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <CorrespondenceCard 
+      v-for="correspondence in correspondences"
+      :names='correspondence.names' 
+      :backgroundIds='correspondence.backgroundIds' 
+      :numberLetters='correspondence.numberLetters' 
+      :curatorialStatement='correspondence.curatorialStatement'
+      class="ma3 dib v-top"
+    />
+  </section>
+</template>
+
+<script>
+import CorrespondenceCard from "./CorrespondenceCard";
+export default {
+  name: "CorrespondenceList",
+  components: {
+    CorrespondenceCard,
+  },
+  props: {
+    correspondences: { type: Array, default: () => [] },
+  },
+};
+</script>
