6f53e4dbcc09e1e1a42263f8f6e8a29c4648f15e
Philo van Kemenade <philovankemenade@gmail.com>
2020-03-09 00:57:31 +0100

    constrain bg avatar to not overlap, specified via separationFactor param

--- src/components/AvatarBackground.vue
+++ src/components/AvatarBackground.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="avatar-background absolute w-100 h-100 bg-creme overflow-hidden">
+  <div class="absolute w-100 h-100 bg-creme overflow-hidden z--1">
     <div
       v-if="showExclusionZone"
       class="absolute ba b--red b--dashed o-50"
@@ -11,11 +11,11 @@
       }"
     ></div>
     <AvatarItem
-      v-for="avatar in avatars"
+      v-for="avatar in avatarsHydrated"
       :type="avatar.type"
       :bgImageSrc="avatar.bgImageSrc"
-      class="absolute background-avatar-filter"
-      :style="randomPosExclusiveStyle()"
+      :style="avatar.style"
+      class="absolute filter-lowcontrast"
     />
   </div>
 </template>
@@ -30,6 +30,12 @@ export default {
   data: function() {
     return {
       avatarSize: 150,
+      avatarsHydrated: [],
+      // separationFactor: how much to separate individual avatars?
+      // 0.5 = allow upto half of avatar size to overlap
+      // 1   = allow upto 'border to border' fit
+      // 1.5 = require at least half of avatar size in between
+      separationFactor: 1.1,
     }
   },
   props: {
@@ -54,46 +60,74 @@ export default {
     },
   },
   methods: {
-    randomXPx () {
-      return Math.random() * this.clientWidth
-    },
-    randomYPx () {
-      return Math.random() * this.clientHeight
-    },
     randomPos () {
       return {
-        x: this.randomXPx(),
-        y: this.randomYPx(),
+        x: Math.random() * this.clientWidth,
+        y: Math.random() * this.clientHeight,
       }
     },
     randomPosExclusive () {
       let randomPos = this.randomPos()
-      return this.isInsideExclusion(randomPos)
+      return this.overlaps(randomPos, this.excludeZone)
         ? this.randomPosExclusive()
         : randomPos
     },
-    randomPosExclusiveStyle () {
-      let randomPosExclusive = this.randomPosExclusive()
+    getStyleForPos (pos) {
       return { 
-        left: randomPosExclusive.x - (0.5 * this.avatarSize) + 'px',
-        top: randomPosExclusive.y - (0.5 * this.avatarSize) + 'px',
+        left: pos.x - (0.5 * this.avatarSize) + 'px',
+        top: pos.y - (0.5 * this.avatarSize) + 'px',
       }
     },
-    isInsideExclusion (pos) {
-      return pos.x > this.excludeZone.minX &&
-             pos.x < this.excludeZone.maxX &&
-             pos.y > this.excludeZone.minY &&
-             pos.y < this.excludeZone.maxY
+    overlaps (pos, excludeArea) {
+      return pos.x > excludeArea.minX &&
+             pos.x < excludeArea.maxX &&
+             pos.y > excludeArea.minY &&
+             pos.y < excludeArea.maxY
     },
+    overlapsPositions (candidatePos, existingPositions) {
+      return existingPositions.some(pos => {
+        return this.overlaps(candidatePos, this.getAreaForPos(pos, this.avatarSize))
+      })
+    },
+    getAreaForPos (pos, size) {
+      return {
+        minX: pos.x - this.separationFactor * size,
+        maxX: pos.x + this.separationFactor * size,
+        minY: pos.y - this.separationFactor * size,
+        maxY: pos.y + this.separationFactor * size
+      }
+    },
+    hydrateOneAvatar (avatar) {
+      let candidatePos = this.randomPosExclusive()
+      let existingPositions = this.avatarsHydrated.map(a => a.pos)
+      if (!this.overlapsPositions(candidatePos, existingPositions)) {
+        this.avatarsHydrated.push({
+          type: avatar.type,
+          bgImageSrc: avatar.bgImageSrc,
+          style: this.getStyleForPos(candidatePos),
+          pos: candidatePos
+        })
+      } else {
+        this.hydrateOneAvatar(avatar)
+      }
+    },
+    hydrateAvatars () {
+      this.avatars.forEach(avatar => {
+        this.hydrateOneAvatar(avatar)
+      })
+    },
+  },
+  created () {
+    this.hydrateAvatars()
   },
 };
 </script>
 
 <style scoped>
-.avatar-background {
+.z--1 {
   z-index: -1;
 }
-.background-avatar-filter {
+.filter-lowcontrast {
   filter: contrast(40%) brightness(150%) saturate(75%);
 }
 </style>
