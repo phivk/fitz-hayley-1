bd000989035b6d899b0141237e884ba0d1aa9afb
Alexa S <alexasteinbrueck@gmail.com>
2020-03-09 10:55:19 +0100

    Use styled font in breadcrumbs

--- src/components/TheBreadcrumbs.vue
+++ src/components/TheBreadcrumbs.vue
@@ -1,40 +1,43 @@
 <template>
-   <div>
-      <span v-for="(crumb, index) in crumbs" :key="crumb.path">
-        <a :href="crumb.path" class="mr2 v-mid">{{crumb.text}}</a>
-        <span v-if="index !== crumbs.length-1" class="mr2 v-mid">➺</span>
-      </span>
-    </div> 
+  <div>
+    <span v-for="(crumb, index) in crumbs" :key="crumb.path">
+      <a :href="crumb.path" class="mr2 v-mid serif f6">{{ crumb.text }}</a>
+      <span v-if="index !== crumbs.length - 1" class="mr2 v-mid">➺</span>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TheBreadcrumbs",
-  data: function () {
-    return {}
+  data: function() {
+    return {};
   },
-  props: { 
-    pathList: { type: Array, default: () => undefined },
+  props: {
+    pathList: { type: Array, default: () => undefined }
   },
   computed: {
-    crumbs () {
-      return [{text: "Home", path: "/"}].concat(         
-        this.pathList 
-        || window.location.pathname.split('/')
+    crumbs() {
+      return [{ text: "Home", path: "/" }].concat(
+        this.pathList ||
+          window.location.pathname
+            .split("/")
             .slice(1)
-            .map((path, index, paths) => { 
+            .map((path, index, paths) => {
               return {
-                'text': this.capitalise(path.split('.')[0]),
-                'path': paths.slice(0, index+1).join('/')
-              }
+                text: this.capitalise(path.split(".")[0]),
+                path: paths.slice(0, index + 1).join("/")
+              };
             })
-      )
+      );
     }
   },
   methods: {
-    capitalise (string) {
-      return typeof string !== 'string' ? '' : string.charAt(0).toUpperCase() + string.slice(1)
+    capitalise(string) {
+      return typeof string !== "string"
+        ? ""
+        : string.charAt(0).toUpperCase() + string.slice(1);
     }
-  },
+  }
 };
 </script>
